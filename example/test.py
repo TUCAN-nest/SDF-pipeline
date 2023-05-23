b2853b5716caf3b649d067a6179b060e02f2c305
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-05-23 09:24:42 +0000

    Add example

---
+++ example/test.py
@@ -0,0 +1,42 @@
+import re
+from typing import Final, Callable
+from sdf_pipeline import drivers, utils
+
+SDF_PATH: Final = "example/mcule_2000.sdf.gz"
+
+
+def _regression_consumer(molfile: str, get_molfile_id: Callable):
+    return (
+        "regression",
+        utils.get_current_time(),
+        get_molfile_id(molfile),
+        f"result for {get_molfile_id(molfile)}",
+    )
+
+
+def _get_mcule_id(molfile: str) -> str:
+    molfile_id_pattern = re.compile(r"<Mcule_ID>(.*?)>", re.DOTALL)
+    molfile_id_match = molfile_id_pattern.search(molfile)
+    molfile_id = molfile_id_match.group(1).strip() if molfile_id_match else ""
+    return molfile_id
+
+
+if __name__ == "__main__":
+    args = drivers.parse_cli_args()
+
+    if args.test_type == "regression":
+        if args.compute_reference_result:
+            drivers.regression_reference(
+                sdf_path=SDF_PATH,
+                log_path=args.result_destination,
+                consumer_function=_regression_consumer,
+                get_molfile_id=_get_mcule_id,
+            )
+        else:
+            drivers.regression(
+                sdf_path=SDF_PATH,
+                log_path=args.result_destination,
+                reference_path=args.reference_result,
+                consumer_function=_regression_consumer,
+                get_molfile_id=_get_mcule_id,
+            )
