2131585c560d83509717a22a0c1028cb8e6a99b7
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-10-27 14:08:27 +0000

    Raise exception for processes that terminate with non-zero exit code
    Prior to this commit, `core.run` would wait for results indefinitely
    when a child process terminated with a non-zero exit code (e.g., segfault).
    This was due to the fact that `multiprocessing.Pool` does not raise
    an exception ( https://github.com/python/cpython/issues/66587 ). This commit
    replaces `multiprocessing.Pool` with `concurrent.futures.ProcessPoolExecutor`.
    In contrast to the former, the latter raises an exception in case a
    child process terminates with a non-zero exit code.

--- src/sdf_pipeline/core.py
+++ src/sdf_pipeline/core.py
@@ -1,7 +1,8 @@
-import multiprocessing
 import gzip
+from concurrent.futures import ProcessPoolExecutor, as_completed, process
 from typing import Callable, TYPE_CHECKING
 from collections.abc import Generator
+from sdf_pipeline import logger
 
 if TYPE_CHECKING:
     # https://adamj.eu/tech/2021/05/13/python-type-hints-how-to-fix-circular-imports/
@@ -13,75 +14,58 @@ def read_records_from_gzipped_sdf(sdf_path: str) -> Generator[str, None, None]:
     current_record = ""
     # TODO: guard file opening.
     with gzip.open(sdf_path, "rb") as gzipped_sdf:
-        # decompress SDF line-by-line to avoid loading entire SDF into memory
+        # Decompress SDF line-by-line to avoid loading entire SDF into memory.
         for decompressed_line in gzipped_sdf:
             decoded_line = decompressed_line.decode("utf-8", "backslashreplace")
             current_record += decoded_line
             if decoded_line.strip() == "$$$$":
                 # TODO: harden SDF parsing according to
-                # http://www.dalkescientific.com/writings/diary/archive/2020/09/18/handling_the_sdf_record_delimiter.html
+                # http://www.dalkescientific.com/writings/diary/archive/2020/09/18/handling_the_sdf_record_delimiter.html.
                 yield current_record
                 current_record = ""
 
-
-def _produce_molfiles(
-    molfile_queue: multiprocessing.Queue, sdf_path: str, n_poison_pills: int
-) -> None:
-    for molfile in read_records_from_gzipped_sdf(sdf_path):
-        molfile_queue.put(molfile)
-
-    for _ in range(n_poison_pills):
-        molfile_queue.put("DONE")  # poison pill: tell consumer processes we're done
-
-
-def _consume_molfiles(
-    molfile_queue: multiprocessing.Queue,
-    result_queue: multiprocessing.Queue,
-    consumer_function: Callable,
-) -> None:
-    for molfile in iter(molfile_queue.get, "DONE"):
-        result_queue.put(consumer_function(molfile))
-
-    result_queue.put(f"DONE")
+    return None
 
 
 def run(
-    sdf_path: str,
-    consumer_function: Callable,
-    number_of_consumer_processes: int,
+    sdf_path: str, consumer_function: Callable, number_of_consumer_processes: int
 ) -> Generator["ConsumerResult", None, None]:
-    molfile_queue: multiprocessing.Queue = multiprocessing.Queue()  # TODO: limit size?
-    result_queue: multiprocessing.Queue = multiprocessing.Queue()
 
-    producer_process = multiprocessing.Process(
-        target=_produce_molfiles,
-        args=(molfile_queue, sdf_path, number_of_consumer_processes),
-    )
-    producer_process.start()
+    try:
+        with ProcessPoolExecutor(
+            max_workers=number_of_consumer_processes
+        ) as process_pool:
+            futures = (
+                process_pool.submit(consumer_function, molfile)
+                for molfile in read_records_from_gzipped_sdf(sdf_path)
+            )
+            """
+            `as_completed` isn't iterating `futures` lazily. It instantiates a set of futures:
+            https://github.com/python/cpython/blob/f383ca1a6fa1a2a83c8c1a0e56cf997c77fa2893/Lib/concurrent/futures/_base.py#L220.
+            In case of memory constraints, consider chunking `futures`: https://bugs.python.org/issue34168.
+            """
+            for future in as_completed(futures):
+                exception = future.exception()
+                if exception is not None:
+                    logger.error(f"could not process {sdf_path}: {exception}")
 
-    consumer_processes = [
-        multiprocessing.Process(
-            target=_consume_molfiles,
-            args=(
-                molfile_queue,
-                result_queue,
-                consumer_function,
-            ),
-        )
-        for process_id in range(number_of_consumer_processes)
-    ]
-    for consumer_process in consumer_processes:
-        consumer_process.start()
+                    raise exception  # Dead programs don't tell lies.
 
-    number_of_finished_consumer_processes = 0
-    while number_of_finished_consumer_processes < number_of_consumer_processes:
-        result = result_queue.get()  # blocks until result is available
-        if result == "DONE":
-            number_of_finished_consumer_processes += 1
-            continue
-        yield result
+                yield future.result()
 
-    # processes won't join before all queues their interacting with are empty
-    producer_process.join()
-    for consumer_process in consumer_processes:
-        consumer_process.join()
+    except process.BrokenProcessPool as exception:
+        """
+        `exception` can be thrown in two ways with (slightly) different messages:
+        1) While processing a future in the `as_completed` loop above,
+            in which case `exception` is re-raised below.
+            See https://github.com/python/cpython/blob/7f074a771bc4e3e299799fabf9b054a03f6693d2/Lib/concurrent/futures/process.py#L489
+            and https://discuss.python.org/t/as-completed-not-yielding-futures-that-raised-until-pool-exits/39920.
+        2) While submitting a task to the `process_pool`.
+            See https://github.com/python/cpython/blob/7f074a771bc4e3e299799fabf9b054a03f6693d2/Lib/concurrent/futures/process.py#L793.
+        We need this `except` block in order to catch 2). For 1), the inner `raise` would be sufficient.
+        """
+        logger.error(f"could not process {sdf_path}: {exception}")
+
+        raise exception
+
+    return None
