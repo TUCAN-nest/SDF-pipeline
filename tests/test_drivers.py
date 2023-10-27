0b867a0392344777d4a9a065d6628087793a8e53
Jan C. Brammer <jan.c.brammer@gmail.com>
2023-10-27 14:08:27 +0000

    Raise exception for processes that terminate with non-zero exit code
    Prior to this commit, `core.run` would wait for results indefinitely
    when a child process terminated with a non-zero exit code (e.g., segfault).
    This was due to the fact that `multiprocessing.Pool` does not raise
    an exception ( https://github.com/python/cpython/issues/66587 ). This commit
    replaces `multiprocessing.Pool` with `concurrent.futures.ProcessPoolExecutor`.
    In contrast to the former, the latter raises an exception in case a
    child process terminates with a non-zero exit code.

--- tests/test_drivers.py
+++ tests/test_drivers.py
@@ -3,21 +3,44 @@ import sqlite3
 import pytest
 import logging
 import json
+import ctypes
+import timeit
 from operator import add
 from functools import reduce
 from pathlib import Path
 from typing import Callable
-from sdf_pipeline import drivers
+from functools import partial
+from concurrent.futures.process import BrokenProcessPool
+from sdf_pipeline import drivers, core
 
 
-def regression_consumer(
-    molfile: str, get_molfile_id: Callable
-) -> drivers.ConsumerResult:
+def consumer(molfile: str, get_molfile_id: Callable) -> drivers.ConsumerResult:
     return drivers.ConsumerResult(
         get_molfile_id(molfile),
         "regression",
         str(len(molfile)),
     )
+
+
+def busy_consumer(molfile: str, get_molfile_id: Callable) -> drivers.ConsumerResult:
+    n = 0
+    for i in range(100):
+        n += sum([len(line) ** i for line in molfile.split("\n")])
+    return drivers.ConsumerResult(
+        get_molfile_id(molfile),
+        "regression",
+        n,
+    )
+
+
+def segfaulting_consumer(
+    molfile: str, get_molfile_id: Callable
+) -> drivers.ConsumerResult:
+    ctypes.string_at(0)
+
+
+def raising_consumer(molfile: str, get_molfile_id: Callable) -> drivers.ConsumerResult:
+    1 / 0
 
 
 @pytest.fixture
@@ -45,7 +68,7 @@ def test_regression_reference_driver(sdf_path, tmp_path):
     exit_code = drivers.regression_reference(
         sdf_path=sdf_path,
         reference_path=tmp_path / "regression_reference.sqlite",
-        consumer_function=regression_consumer,
+        consumer_function=consumer,
         get_molfile_id=_get_mcule_id,
         number_of_consumer_processes=2,
     )
@@ -63,7 +86,7 @@ def test_regression_driver(sdf_path, reference_path, caplog):
     exit_code = drivers.regression(
         sdf_path=sdf_path,
         reference_path=reference_path,
-        consumer_function=regression_consumer,
+        consumer_function=consumer,
         get_molfile_id=_get_mcule_id,
         number_of_consumer_processes=2,
     )
@@ -77,3 +100,59 @@ def test_regression_driver(sdf_path, reference_path, caplog):
         "info": "regression",
         "diff": '{"current": "926", "reference": "42"}',
     }
+
+
+def test_core_raises_on_exception(sdf_path, caplog):
+    caplog.set_level(logging.ERROR, logger="sdf_pipeline")
+    with pytest.raises(ZeroDivisionError):
+        for _ in core.run(
+            sdf_path=str(sdf_path),
+            consumer_function=partial(raising_consumer, get_molfile_id=_get_mcule_id),
+            number_of_consumer_processes=2,
+        ):
+            pass
+    assert (
+        caplog.records[-1].message == f"could not process {sdf_path}: division by zero"
+    )
+
+
+def test_core_raises_on_segfault(sdf_path, caplog):
+    caplog.set_level(logging.ERROR, logger="sdf_pipeline")
+    with pytest.raises(BrokenProcessPool):
+        for _ in core.run(
+            sdf_path=str(sdf_path),
+            consumer_function=partial(
+                segfaulting_consumer, get_molfile_id=_get_mcule_id
+            ),
+            number_of_consumer_processes=2,
+        ):
+            pass
+    # See comment in `core.run` for why there's two alternative messages.
+    assert caplog.records[-1].message in [
+        f"could not process {sdf_path}: A process in the process pool was terminated abruptly while the future was running or pending.",
+        f"could not process {sdf_path}: A child process terminated abruptly, the process pool is not usable anymore",
+    ]
+
+
+@pytest.mark.skip
+def test_performance(sdf_path):
+    def run_core(n_processes):
+        def _run_core():
+            for _ in core.run(
+                sdf_path=str(sdf_path),
+                consumer_function=partial(busy_consumer, get_molfile_id=_get_mcule_id),
+                number_of_consumer_processes=n_processes,
+            ):
+                pass
+
+        return _run_core
+
+    previous_execution_time = 0
+    for n in [1, 2, 4, 8, 16]:
+        execution_time = timeit.Timer(run_core(n)).timeit(1)
+        print(f"{n} processes finished in {execution_time} seconds.")
+
+        if previous_execution_time:
+            assert execution_time < previous_execution_time
+
+        previous_execution_time = execution_time
